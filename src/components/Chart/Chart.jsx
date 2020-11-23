0faf0b11ac582368b118c771295aeb3ef7bb62f1
jaimevepe <jaime.velazquez@g.austincc.edu>
2020-11-23 14:46:10 -0600

    adjusted font size from Bar chart/options to 24

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -57,7 +57,7 @@ const Chart = ({data: {confirmed, recovered, deaths}, country }) => {
           }}
           options = {{
             legend: { display: false },
-            title: { display: true, text: `Current state in ${country}` }
+            title: { display: true, fontSize: 24, text: `Current state in ${country}` }
           }}
         />
       ) : null
